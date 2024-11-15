a5e8156ec7b597173af86388e92939f1ddafe69c
Jean Lozano <jmlozano@itc.com.co>
2024-11-15 15:56:45 -0500

    Start project and initial setup (db and swagger)

---
+++ src/main/java/co/com/accenture/testaccenturebackend/TestAccentureBackendApplication.java
@@ -0,0 +1,13 @@
+package co.com.accenture.testaccenturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAccentureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAccentureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/accenture/testaccenturebackend/TestAccentureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.accenture.testaccenturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAccentureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
